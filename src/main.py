5f2d67be897bed2d905e1b1414cd728b07202b1d
RamazanImashov <ramazanimashov@gmil.com>
2023-12-18 21:32:23 +0600

    get id

--- src/main.py
+++ src/main.py
@@ -29,3 +29,15 @@ def send_email_client(background_tasks: BackgroundTasks, tasks: schemas.TasksCre
     session.add(tasks)
     session.commit()
     return tasks
+
+
+@app.get('/get_task', response_model=list[schemas.Tasks], status_code=status.HTTP_200_OK)
+def get_task(session: Session = Depends(get_session)):
+    task = session.query(models.Tasks).all()
+    return task
+
+
+@app.get('/get_task/{task_id}', response_model=list[schemas.Tasks], status_code=status.HTTP_200_OK)
+def get_task_id(task_id: int, session: Session = Depends(get_session)):
+    tasks = session.query(models.Tasks).all()
+    return [tas for tas in tasks if tas.id == task_id]
